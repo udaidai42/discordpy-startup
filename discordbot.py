d3a0460006624ca79f65b9b0d889a02590fe75cc
udaidai42 <56784123+udaidai42@users.noreply.github.com>
2020-05-29 22:42:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+async def anal(ctx):
+    await ctx.sent('anal')
 
 bot.run(token)
