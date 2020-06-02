855703fafe11963fa529681c44d64901932838ab
udaidai42 <56784123+udaidai42@users.noreply.github.com>
2020-06-03 06:30:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,16 @@ import traceback
 bot = commands.Bot(command_prefix='k.')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.event
+async def on_message(message):
+    try:
+        if message.author.bot:
+            return
+        await bot.process_commands(message)
+    except Exception:
+        await message.channel.send(f'```\n{traceback.format_exc()}\n```')
+
+
 @bot.commands
 async def test(ctx):
     await ctx.send('おまんこ')
