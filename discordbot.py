6f942220f4bd118e7be8e32ba8d367f0a34a9225
udaidai42 <56784123+udaidai42@users.noreply.github.com>
2020-06-03 06:29:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,9 +6,9 @@ import traceback
 bot = commands.Bot(command_prefix='k.')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.commands
+async def test(ctx):
+    await ctx.send('おまんこ')
 
-@bot.command
-async def test(message):
-    await message.send('p')
 
 bot.run(token)
